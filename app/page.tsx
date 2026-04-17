656f9d4a013198dfe51d64dca8d9b71b3bb59d36
Jason Porman <Japorma101@gmail.com>
2026-04-17 17:18:34 +1000

    update site

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <h1 className="mt-5 text-5xl sm:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tight">
               Scan your look.
               <span className="block bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
-                Build your pharmaceutical plan.
+                Build your Pharma plan.
               </span>
             </h1>
 
